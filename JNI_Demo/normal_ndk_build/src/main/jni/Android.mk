1a1e47c5f06d28532a7bc98a0c48e8d2d17e3a4d
luyanhao <luyanhao@baidu-mgame.com>
2016-12-27 17:54:28 +0800

    android.mk 配置cpp文件路径

--- JNI_Demo/normal_ndk_build/src/main/jni/Android.mk
+++ JNI_Demo/normal_ndk_build/src/main/jni/Android.mk
@@ -5,10 +5,10 @@ LOCAL_PATH:=$(callmy-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE:=JNI_ANDROID_TEST
-LOCAL_SRC_FILES:=E:\work\studio_work\JNI_Study\normal_ndk_build\src/main/jni/JniTest.cpp
+LOCAL_SRC_FILES:=E:\luyanhao\mygithub\JNI_Demo\JNI_Demo\normal_ndk_build\src\main\jni\JniTest.cpp
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE:=JNI_DYNAMIC_ANDROID_TEST
-LOCAL_SRC_FILES:=E:\work\studio_work\JNI_Study\normal_ndk_build\src/main/jni/JNIDynamicUtils.cpp
+LOCAL_SRC_FILES:=E:\luyanhao\mygithub\JNI_Demo\JNI_Demo\normal_ndk_build\src\main\jni\JNIDynamicUtils.cpp
 include $(BUILD_SHARED_LIBRARY)
